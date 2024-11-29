5a3812e688fd7b2f19f630574b672de591f20aab
sssallias <top.chel.05@mail.ru>
2024-11-29 12:30:08 +0300

    imba news

--- src/widgets/news-list/ui/news-list.tsx
+++ src/widgets/news-list/ui/news-list.tsx
@@ -1,9 +1,21 @@
 import { NewsItem } from 'src/entities/news'
+import { News } from 'src/services-api'
+
+const news: News[] = [
+    {
+        id: 'afsafsaf',
+        title: 'Крутая новость',
+        description: 'Очень крутая новость',
+        publish_data: '29.11.2024',
+        media: [],
+        organization_id: 'asfsaf'
+    }
+]
 
 const NewsList: React.FC = () => {
     return (
         <div className='flex flex-col gap-2'>
-            <NewsItem/>
+            {news.map(dto => <NewsItem dto={dto}/>)}
         </div>
     )
 }
